42dec0a661e21c5ae1455b375a9827782862d1db
Robinson <gnrn@protonmail.ch>
2019-11-18 05:57:23 -0800

    Recovery: Working well Vuetify and Routes.

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>
+          <router-link to="/">Home</router-link>
+        </v-btn>
+        <v-btn flat>
+          <router-link to="/about">About</router-link>
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+//import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+
+  components: {
+    //HelloWorld
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
