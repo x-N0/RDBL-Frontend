98ff354371c45dbe21a7d63a5ab1c12824f9eed2
Robinson <robinsonkaplan@gmail.com>
2019-11-18 06:09:29 -0800

    Refactor: Add Vuetify Minify.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,28 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    dark: true,
+    themes: {
+      light: {
+        primary: "#00bcd4",
+        secondary: "#8bc34a",
+        accent: "#cddc39",
+        error: "#ff5722",
+        warning: "#f44336",
+        info: "#607d8b",
+        success: "#4caf50"
+      }
+    },
+    options: {
+      minifyTheme: function(css) {
+        return process.env.NODE_ENV === "production"
+          ? css.replace(/[\r\n|\r|\n]/g, "")
+          : css;
+      }
+    }
+  }
 });
