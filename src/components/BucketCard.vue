8bf23be36d718f5521463734b036b9ecc4710f36
Robinson <robinsonkaplan@gmail.com>
2019-11-20 12:11:03 -0800

    Refactor: Implement Cards, Toolbar and Menu Toolbar for sm viewports, test ImgSlider.

---
+++ src/components/BucketCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card class="mx-auto" max-width="400">
+    <v-img class="white--text align-end" height="200px" src="">
+      <v-card-title>{{ BucketName }}</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-0"
+      >This experience is on top: {{ BucketPosition }}</v-card-subtitle
+    >
+
+    <v-card-text class="text--primary">
+      <div>{{ BucketPlace }}</div>
+      <div>{{ BucketDescript }}</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="primary" text>
+        Tell a Friend
+      </v-btn>
+
+      <v-btn color="accent" text>
+        Explore
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "BucketCard",
+  props: {
+    BucketPhoto: null,
+    BucketName: null,
+    BucketPosition: null,
+    BucketPlace: null,
+    BucketDescript: null
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Hamburger.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="ham">
+    <v-navigation-drawer v-model="drawer" fixed temporary app>
+      <v-list dense>
+        <v-list-item
+          @click.stop="left = !left"
+          v-for="(item, i) in this.$router.options.routes"
+          :key="i"
+          :to="item.path"
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.name }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.name }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hamburger",
+  props: {
+    drawer: null
+  },
+  data() {
+    return {
+      left: false
+    };
+  }
+};
+</script>
+
+<style scoped></style>
