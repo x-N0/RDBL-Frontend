ea430d0df07a2d5e536662853eec75410356cb69
Robinson <robinsonkaplan@gmail.com>
2019-11-27 15:38:23 -0800

    Refactor: (APP) Modify the APP main structure to Toolbar|Router-V|Footer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <v-app>
-    <Toolbar />
-    <v-content>
-      <v-container fluid>
-        <router-view />
-      </v-container>
-    </v-content>
-
-    <v-footer app color="blue-grey" class="white--text">
-      <span>Vuetify</span>
-      <v-spacer></v-spacer>
-      <span>&copy; 2019</span>
-    </v-footer>
-  </v-app>
+  <div class="app">
+    <v-app id="inspire">
+      <Toolbar />
+      <v-content>
+        <v-container fluid>
+          <router-view />
+        </v-container>
+      </v-content>
+      <AppFooter></AppFooter>
+    </v-app>
+  </div>
 </template>
 
 <script>
-  import Toolbar from "./components/Toolbar";
+  import Toolbar from "./components/AppToolbar";
+  import AppFooter from "./components/AppFooter";
 
   export default {
   name: "App",
   components: {
-    Toolbar
+    Toolbar,
+    AppFooter
   },
-  data: () => ({})
+  data: () => ({}),
+  propsTypes: {
+    Str: { type: [String] },
+    Int: { type: [Number] },
+    StrReq: { type: [String], required: true },
+    IntReq: { type: [Number], required: true }
+  }
 };
 </script>
+<style scoped lang="scss">
+@import "assets/scss/main";
+input[type="text"],
+input[type="number"],
+select,
+textarea {
+  font-size: $fontSize;
+}
+</style>
