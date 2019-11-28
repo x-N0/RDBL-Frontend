acd3355e3334d9db9bbd6fadb8917c9506b80c91
Robinson <gnrn@protonmail.ch>
2019-11-28 14:56:58 -0800

    Feat: (Alert) add Alert Component connected to Vuex Store

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-col class="alerts">
+    <v-alert
+      v-for="alert in alerts"
+      :key="alert.id"
+      class="text-center"
+      :type="alert.type"
+      dismissible
+      border="left"
+      @click="closeAlert"
+      >{{ alert.msg }}</v-alert
+    >
+  </v-col>
+</template>
+
+<script>
+import { mapMutations, mapState } from "vuex";
+
+export default {
+  name: "AppError",
+  computed: {
+    ...mapState(["alerts"])
+  },
+  methods: {
+    ...mapMutations(["setAlert"]),
+    closeAlert() {
+      this.setAlert({});
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.alerts {
+  position: fixed;
+  z-index: 101;
+}
+</style>
