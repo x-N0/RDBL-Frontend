b177eb758ad0d421867bfee9cf76acc3bbead858
Robinson <gnrn@protonmail.ch>
2019-11-28 14:53:37 -0800

    Feat: (Cards) Add Loading Skeletons

--- src/components/SkeletonsLoads/BucketCardSKE.vue
+++ src/components/SkeletonsLoads/BucketCardSKE.vue
@@ -4,7 +4,11 @@
       <v-skeleton-loader type="image"></v-skeleton-loader>
     </v-img>
     <v-card-title>
-      <v-skeleton-loader type="heading" width="20em" height="2em"></v-skeleton-loader>
+      <v-skeleton-loader
+        type="heading"
+        width="20em"
+        height="2em"
+      ></v-skeleton-loader>
     </v-card-title>
     <v-card-subtitle class="pb-3">
       <v-skeleton-loader type="text" width="5em"> </v-skeleton-loader>
@@ -14,7 +18,11 @@
     </v-card-text>
     <v-card-actions class="align-end">
       <v-skeleton-loader type="button" width="10em"></v-skeleton-loader>
-      <v-skeleton-loader class="ml-5" type="button" width="12em"></v-skeleton-loader>
+      <v-skeleton-loader
+        class="ml-5"
+        type="button"
+        width="12em"
+      ></v-skeleton-loader>
     </v-card-actions>
   </v-card>
 </template>
